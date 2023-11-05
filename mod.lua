ec2c1aae8f99e413daf17128873ced265b64da6e
Florian Thiel <flth@flth.de>
2023-11-05 20:49:25 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,58 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "train", "siemens", "es64f4", "eurosprinter", "electric", "multi-system", "universal" },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+        params = {
+			{
+				key = "g1206fake",
+				name = _("Fake_g1206"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_g_desc"),
+				defaultIndex = 0,
+			},
+        },
+	},
+	options = {
+	},
+
+	runFn = function (settings, modParams)
+
+		--- Fake ---
+		
+        local fake_g1206 = {
+			["fake_gebraucht1.mdl"] = true,
+			["fake_gebraucht2.mdl"] = true,
+        }
+        
+		--- Fake --
+        local modelFilterFake = function(fileName, data)
+			local modelName = fileName:match('/g1206_([^/]*.mdl)')
+			return (modelName==nil or fake_g1206[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+            if params["g1206fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilterFake)
+			end
+		else
+            addFileFilter("model/vehicle", modelFilterFake)
+		end
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,77 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Mak G1206",
+		["mod_desc"] = "The G1206 is a diesel hydraulic for heavy shunting and mainline service. The locomotive was developed and built in 1997 by Mak (Maschinenbau Kiel). In 1998 Mak was bought by Vossloh, since then the locomotives have been running under the Vossloh G1206.",
+
+        ["g1206_menu"] = "Mak G1206",
+        ["g1206_fake_menu"] = "Mak G1206 Fakes",
+		["g1206_desc"] = "The G1206 is a diesel hydraulic for heavy shunting and mainline service. The locomotive was developed and built in 1997 by Mak (Maschinenbau Kiel). In 1998 Mak was bought by Vossloh, since then the locomotives have been running under the Vossloh G1206.",
+
+		["klima_name"] = "With air conditioning on the roof",
+
+		--- Standard ---
+		["g1206_avg_name"] = 			"1275 506 AVG",
+
+        -- Fake
+        ["Fake_g1206"] = "Active fake engines?",
+        ["option_fake_g1206_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
+
+        ["g1206_avg_name_fake"] = "Fake - 1275 506 AVG",
+
+	},
+	de = {
+
+		["mod_name"] = "MaK G 1206",
+		["mod_desc"] = "Die G 1206 ist eine dieselhydraulische Lokomotive  für den schweren Rangier- und Streckendienst. Die Lok wurde 1997 von MaK (Maschinenbau Kiel) entwickelt und gebaut. 1998 wurde MaK von Vossloh aufgekauft, seitdem laufen die Loks unter dem Namen Vossloh G 1206.",
+
+        ["g1206_menu"] = "MaK G 1206",
+        ["g1206_fake_menu"] = "MaK G 1206 Fakes",
+		["g1206_desc"] = "Die G 1206 ist eine dieselhydraulische Lokomotive  für den schweren Rangier- und Streckendienst. Die Lok wurde 1997 von MaK (Maschinenbau Kiel) entwickelt und gebaut. 1998 wurde MaK von Vossloh aufgekauft, seitdem laufen die Loks unter dem Namen Vossloh G 1206.",
+
+		["klima_name"] = "Mit Klimaanlage auf dem Dach",
+
+		--- Standard ---
+		["g1206_avg_name"] = 			"1275 506 AVG (2007)",
+		["g1206_chemoil_name"] = 		"1275 009 Chemoil SBB Cargo International (2014)",
+		["g1206_wle_name"] = 			"1275 505 WLE (2007)",
+
+		--- Ideeen
+		["g1206_sbb_name"] = 			"1276 005 EVB/SBB Cargo International",
+		["g1206_captrain_name"] = 		"1275 801 Captrain",
+		["g1206_rbh_name"] = 			"1275 805 RBH",
+		["g1206_evb_name"] = 			"1275 503 Elbe-Weserbahn",
+		["g1206_evb2_name"] = 			"1275 102 Elbe-Weserbahn 2",
+		["g1206_mwb_name"] = 			"1275 102 Mittelweserbahn",
+		["g1206_hgb_name"] = 			"1275 501 HGB Frankenbach",
+		["g1206_alpha_name"] = 			"1275 119 Alpha Trains",
+		["g1206_meg_name"] = 			"1275 212 MEG",
+		["g1206_esg_name"] = 			"1275 022 Eisenbahn-Service-Gesellschaft",
+		["g1206_nrail_name"] = 			"1275 804 NorhtRail",
+		["g1206_db_name"] = 			"1275 615 DB/MRCE",
+		["g1206_mrce_name"] = 			"1276 034 MRCE",
+		["g1206_vossloh_name"] = 		"V157 Vossloh",
+		["g1206_lineas_name"] = 		"500 1571 Lineas",
+
+		--- Gedreht ---
+		["g1206_avg_gedreht_name"] = "1275 506 AVG [gedreht] (2007)",
+		["g1206_chemoil_gedreht_name"] = "1275 009 Chemoil SBB Cargo International [gedreht] (2014)",
+		["g1206_wle_gedreht_name"] = "1275 505 WLE [gedreht] (2007)",
+
+        -- Gedreht und Fake
+        ["g1206_avg_gedreht_name"] = "1275 506 AVG [gedreht] [Fake] (2007)",
+		["g1206_chemoil_gedreht_name"] = "1275 009 Chemoil SBB CI [gedreht] [Fake] (2014)",
+		["g1206_wle_gedreht_name"] = "1275 505 WLE [gedreht] [Fake] (2007)",
+
+        -- Fake
+        ["Fake_g1206"] = "Fake-Loks aktivieren?",
+        ["option_fake_g1206_desc"] = "Die Fake-Loks (Motor aus, kein Licht, kein Fahrer) aktiviert werden?",
+
+		["g1206_avg_fake_name"] = "1275 506 AVG [Fake] (2007)",
+		["g1206_chemoil_fake_name"] = "1275 009 Chemoil SBB Cargo International [Fake] (2014)",
+		["g1206_wle_fake_name"] = "1275 505 WLE [Fake] (2007)",
+
+	},
+}
+end
