8d1e4d8728fd30db9cf528d020c5902f61d46f9a
Florian Thiel <flth@flth.de>
2023-12-10 19:52:39 +0100

    fehlende Strings hinzugefügt

--- strings.lua
+++ strings.lua
@@ -33,19 +33,76 @@ return {
 		["klima_name"] = "Mit Klimaanlage auf dem Dach",
 
 		--- Standard ---
+        ["g1206_alpha_name"] =          "Alpha Trains",
 		["g1206_avg_name"] = 			"1275 506 AVG (2007)",
+        ["g1206_blg_rot_name"] =        "BLG rot",
+        ["g1206_captrain_name"] =       "Captrain",
+        ["g1206_cfl_name"] =            "CFL",
 		["g1206_chemoil_name"] = 		"1275 009 Chemoil SBB Cargo International (2014)",
+        ["g1206_duisport_name"] =       "Duisport",
+        ["g1206_lanxess_name"] =        "Lanxess",
+        ["g1206_pool_name"] =           "Pool",
+        ["g1206_rag_blau_name"] =       "RAG blau",
+        ["g1206_rbh_name"] =            "RBH",
+        ["g1206_rurtal_name"] =         "Rurtal",
+        ["g1206_shk_name"] =            "SHK",
 		["g1206_wle_name"] = 			"1275 505 WLE (2007)",
+
+		--- Gedreht ---
+        ["g1206_alpha_gedreht_name"] =          "Alpha Trains",
+		["g1206_avg_gedreht_name"] =            "1275 506 AVG [gedreht] (2007)",
+        ["g1206_blg_rot_gedreht_name"] =        "BLG rot",
+        ["g1206_captrain_gedreht_name"] =       "Captrain",
+        ["g1206_cfl_gedreht_name"] =            "CFL",
+	    ["g1206_chemoil_gedreht_name"] =        "1275 009 Chemoil SBB Cargo International [gedreht] (2014)",
+        ["g1206_duisport_gedreht_name"] =       "Duisport",
+        ["g1206_lanxess_gedreht_name"] =        "Lanxess",
+        ["g1206_pool_gedreht_name"] =           "Pool",
+        ["g1206_rag_blau_gedreht_name"] =       "RAG blau",
+        ["g1206_rbh_gedreht_name"] =            "RBH",
+        ["g1206_rurtal_gedreht_name"] =         "Rurtal",
+        ["g1206_shk_gedreht_name"] =            "SHK",
+		["g1206_wle_gedreht_name"] =            "1275 505 WLE [gedreht] (2007)",
+
+        -- Fake
+        ["g1206_alpha_fake_name"] =          "Alpha Trains",
+		["g1206_avg_fake_name"] = 			"1275 506 AVG (2007)",
+        ["g1206_blg_rot_fake_name"] =        "BLG rot",
+        ["g1206_captrain_fake_name"] =       "Captrain",
+        ["g1206_cfl_fake_name"] =            "CFL",
+		["g1206_chemoil_fake_name"] = 		"1275 009 Chemoil SBB Cargo International (2014)",
+        ["g1206_duisport_fake_name"] =       "Duisport",
+        ["g1206_lanxess_fake_name"] =        "Lanxess",
+        ["g1206_pool_fake_name"] =           "Pool",
+        ["g1206_rag_blau_fake_name"] =       "RAG blau",
+        ["g1206_rbh_fake_name"] =            "RBH",
+        ["g1206_rurtal_fake_name"] =         "Rurtal",
+        ["g1206_shk_fake_name"] =            "SHK",
+		["g1206_wle_fake_name"] = 			"1275 505 WLE (2007)",
+
+        -- Fake gedreht
+        ["g1206_alpha_gedreht_fake_name"] =          "Alpha Trains",
+		["g1206_avg_gedreht_fake_name"] =            "1275 506 AVG [gedreht] (2007)",
+        ["g1206_blg_rot_gedreht_fake_name"] =        "BLG rot",
+        ["g1206_captrain_gedreht_fake_name"] =       "Captrain",
+        ["g1206_cfl_gedreht_fake_name"] =            "CFL",
+	    ["g1206_chemoil_gedreht_fake_name"] =        "1275 009 Chemoil SBB Cargo International [gedreht] (2014)",
+        ["g1206_duisport_gedreht_fake_name"] =       "Duisport",
+        ["g1206_lanxess_gedreht_fake_name"] =        "Lanxess",
+        ["g1206_pool_gedreht_fake_name"] =           "Pool",
+        ["g1206_rag_blau_gedreht_fake_name"] =       "RAG blau",
+        ["g1206_rbh_gedreht_fake_name"] =            "RBH",
+        ["g1206_rurtal_gedreht_fake_name"] =         "Rurtal",
+        ["g1206_shk_gedreht_fake_name"] =            "SHK",
+		["g1206_wle_gedreht_fake_name"] =            "1275 505 WLE [gedreht] (2007)",
+
 
 		--- Ideeen
 		["g1206_sbb_name"] = 			"1276 005 EVB/SBB Cargo International",
-		["g1206_captrain_name"] = 		"1275 801 Captrain",
-		["g1206_rbh_name"] = 			"1275 805 RBH",
 		["g1206_evb_name"] = 			"1275 503 Elbe-Weserbahn",
 		["g1206_evb2_name"] = 			"1275 102 Elbe-Weserbahn 2",
 		["g1206_mwb_name"] = 			"1275 102 Mittelweserbahn",
 		["g1206_hgb_name"] = 			"1275 501 HGB Frankenbach",
-		["g1206_alpha_name"] = 			"1275 119 Alpha Trains",
 		["g1206_meg_name"] = 			"1275 212 MEG",
 		["g1206_esg_name"] = 			"1275 022 Eisenbahn-Service-Gesellschaft",
 		["g1206_nrail_name"] = 			"1275 804 NorhtRail",
@@ -53,24 +110,6 @@ return {
 		["g1206_mrce_name"] = 			"1276 034 MRCE",
 		["g1206_vossloh_name"] = 		"V157 Vossloh",
 		["g1206_lineas_name"] = 		"500 1571 Lineas",
-
-		--- Gedreht ---
-		["g1206_avg_gedreht_name"] = "1275 506 AVG [gedreht] (2007)",
-		["g1206_chemoil_gedreht_name"] = "1275 009 Chemoil SBB Cargo International [gedreht] (2014)",
-		["g1206_wle_gedreht_name"] = "1275 505 WLE [gedreht] (2007)",
-
-        -- Gedreht und Fake
-        ["g1206_avg_gedreht_name"] = "1275 506 AVG [gedreht] [Fake] (2007)",
-		["g1206_chemoil_gedreht_name"] = "1275 009 Chemoil SBB CI [gedreht] [Fake] (2014)",
-		["g1206_wle_gedreht_name"] = "1275 505 WLE [gedreht] [Fake] (2007)",
-
-        -- Fake
-        ["Fake_g1206"] = "Fake-Loks aktivieren?",
-        ["option_fake_g1206_desc"] = "Die Fake-Loks (Motor aus, kein Licht, kein Fahrer) aktiviert werden?",
-
-		["g1206_avg_fake_name"] = "1275 506 AVG [Fake] (2007)",
-		["g1206_chemoil_fake_name"] = "1275 009 Chemoil SBB Cargo International [Fake] (2014)",
-		["g1206_wle_fake_name"] = "1275 505 WLE [Fake] (2007)",
 
 	},
 }
